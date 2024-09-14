84bbf2de0b5a32a6eff01e639f79a82c4418d029
huyvanvu2003 <huyvanvu2003@gmail.com>
2024-09-15 02:41:08 +0700

    Phân chia theo MVC ở backend

--- Shop_Do_Back_End/src/index.js
+++ Shop_Do_Back_End/src/index.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { default: mongoose } = require('mongoose');
+const routes = require('./routes');
+const bodyParser = require('body-parser')
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => {
-    res.send('Hello World 2')
-})
+app.use(bodyParser.json())
 
-console.log('process.env.MONGO_DB', process.env.MONGO_DB)
+routes(app);
 
-mongoose.connect(`mongodb+srv://huyvanvu456:${process.env.MONGO_DB}@cluster0.lktcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
         console.log('Connect Db successfully!')
     })
     .catch(err => {
         console.log(err)
     })
+
 app.listen(port, () => {
     console.log('Server is running in port ', + port)
 })
